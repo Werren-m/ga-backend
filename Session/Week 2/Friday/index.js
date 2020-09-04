a91f6889d05ae8d027571703c29b6d7e7f375554
Vincent Sadino Guizot <vincentguizot128@gmail.com>
2020-09-04 11:25:42 +0700

    morning class

--- Session/Week 2/Friday/index.js
+++ Session/Week 2/Friday/index.js
@@ -1,165 +1,180 @@
-//Deklarasi Variable
-var x = 10;
-
-//Initialisai Variable
-// console.log(x);
-
-var number = 100;
-var nUmBeR = 100;
-var NUMBER = 100;
-
-//Penulisan Variable yg salah
-// var 1 = 1;
-// var 7angka = 0;
-// var list products = [];
-
-//Cara penulisan Variable
+// FPB
 /**
- * Camel Case
- * var listProducts = [];
- * 
- * Snake Case 
- * var list_products = [];
- * 
- * Kebab Case
- * var list-products = [];
- * 
- * Pascal Case
- * var ListProducts = []; 
+ * Algoritma
+ * - Cari definisi FPB
+ * - Cari number min
+ * - Looping maju/mundur dari number min sampai 1
+ * - Pake operator logic &&
+ * - Habis membagi kedua bilangan tsb
  */
 
- //Variable = tempat menampung sebuah data
+// function fpb(number1, number2){
+//     var minNumber;
+//     //Cara Conditional If..else
+//     if(number1 < number2){
+//         minNumber = number1; 
+//     }else {
+//         minNumber = number2;
+//     }
+//     //Cara Ternary Operator
+//     // minNumber = number1 < number2 ? number1 : number2;
+//     for(let i=minNumber; i>=1; i--){
+//         if(number1 % i === 0 && number2 % i === 0){
+//             return i
+//         }
+//     }
+// }
 
-//  TIPE DATA
-//Number
-var angka = 100;
+// console.log(fpb(30,50));
+// console.log(fpb(12,15));
+// console.log(fpb(35,17));
+// console.log(fpb(100,50));
+// console.log(fpb(390,150));
 
-//String
-// var kataMutiara = new String()
-var kata = "Hello World";
-var ANGKA = "100";
+// Print the first n numbers
+/**
+ * Algoritma
+ * - Looping number yg kita input
+ * - Definisi bilangan prima
+ * Bilangan Prima : bilangan yang memiliki faktor 1 dan bilangan itu sendiri 
+ * Faktor = 2
+ * - Print pake while
+ */
+// function checkPrime(number){
+//     // var faktor = [];
+//     var jumlahFaktor = 0;
+//     //code here
+//     for(let i=1; i<=number; i++){
+//         if(number % i === 0){
+//             // faktor.push(i);
+//             jumlahFaktor++;
+//         }
+//     }
+//     if(jumlahFaktor === 2){
+//         return true;
+//     }else {
+//         return false;
+//     }
+//     // return jumlahFaktor;
+// }
+// function print(n){
+//     //code here
+//     var x = 0; 
+//     var number = 1;
 
-//Boolean
-//True or False
-var isOn = true;
-var isWokeUp = false;
-var isHot = true;
-var isWindy = false;
-var isCold = true;
+//     if(n < 1) {
+//         console.log(1)
+//     }else {
+//         while(x < n){
 
-//Null
-var n = null;
-var N = 0;
+//             if(checkPrime(number)){
+//                 console.log(number);
+//                 x++;
+//             }
+//             number++;
+//         }
+//     }
+// }
 
-//Undefined
-var box = undefined;
+//Test Case
+// print(3); //2 3 5
+// print(5); //2 3 5 7 11 
+// print(0); //1
+// console.log(checkPrime(7));
 
-//Javascript all about object
-//Object 
-var mahasiswa = {
-    nama : "Cencent",
-    ipk : 3.21,
-    isAbleCoding : true,
-    computeMath : function(){
-        console.log(100);
+// Mean
+// function mean(array){
+//     var total = 0;
+//     var average;
+//     for(let i=0; i<array.length; i++){
+//         total = total + array[i];
+//     }
+//     average = total / array.length;
+
+//     return average.toFixed(3);
+// }
+
+// console.log(mean([1,2,3,4,5])) //3
+// console.log(mean([3.7,5.3,7.0,1.9,3.1,0.5,1.5])) //3.28 -> 2 
+
+// 'x' and 'o'
+// function checkXO(string) {
+//     //code here
+//     var flagX=0;
+//     var flagO=0;
+//     for (let i = 0; i < string.length; i++) {
+//         if(string[i] === 'x'){
+//             flagX++;
+//         }
+//         else if(string[i] === 'o'){
+//             flagO++;
+//         }
+//     }
+//     if(flagX === flagO){
+//         return 1;
+//     }else {
+//         return -1;
+//     }
+//     // return flagX === flagO ? 1 : -1;
+// }
+
+// //Test
+// console.log(checkXO('xxxxxooooo')) //1
+// console.log(checkXO('xxxooooo')) //-1
+// console.log(checkXO('xoxoxoxoxo')) //1
+
+// Encrypt the string
+
+// function swap(string) {
+//     //code here
+//     // var encrypt = [];
+//     // for (let i = 0; i < string.length; i++) {
+//     //     var ascii = string.charCodeAt(i);
+//     //     encrypt += String.fromCharCode(ascii + 1);
+//     // }
+//     // console.log(encrypt);
+
+//     var huruf = 'abcdefghijklmnopqrstuvwxyz';
+//     var kata = '';
+
+//     for (var i = 0; i < string.length; i++) {
+//         for (var j = 0; j < huruf.length; j++) {
+//             if (string[i] == huruf[j]) {
+//                 if (string[i] == 'z') {
+//                     kata += 'a';
+//                 }
+//                 else kata += huruf[j + 1];
+//             }
+//         }
+//     }
+//     console.log(kata);
+// }
+
+// //Test
+// swap("wow") //xpx
+// swap("javascript") //kbwbtdsjqu
+
+var cities = [
+    {
+        citiesCode : 1,
+        citiesName : "Bangkok"
+    },
+    {
+        citiesCode : 2,
+        citiesName : "Jakarta"
     }
-}
-
-
-//Operasi Math
-// Tambah, Kurang, Kali, Bagi, Modulus
-//Modulus
-// 7/3 = 2 sisa 1
-//7%3 = 1
-// console.log(Math.pow(15,3));
-
-var a = 5;
-var b = 7;
-var total = a + b;
-
-// console.log(total)
-var kata1 = "10"
-var kata2 = true
-
-var kata3 = kata1 + kata2;
-
-// console.log(typeof kata3)
-
-// var subtotal = ((2 * 3 + 1 )- 5) / 2
-// console.log(subtotal)
-
-var p = 7;
-var q = p;
-// q++;
-// q+=1;
-// q=q+1;
-
-// p=q tidak sama q=p
-
-// console.log(p)
-q++
-// console.log(q)
-
-// Tipe Angka
-/**
- * 1. Decimal (10)
- * 0 - 9
- * 
- * 2. Biner (2)
- * 0 1
- * 
- * 1 byte = 8 angka biner/bit , contoh :
- * 01100011 -> 8 bit = 1 byte,
- * 1010001101011100 -> 16 bit = 2 byte,
- * 
- * 3. Octal (8)
- * 0 - 7 
- * 
- * 4. Hexadecimal (16)
- * 0 - 9 A B C D E F
- */
-
-// Logika Math
-//  P      Q       P && Q      P || Q      !P
-//  True   True    True        True        False 
-//  True   False   False       True        False
-//  False  True    False       True        True
-//  False  False   False       False       True
-
-//ARRAY 
-
-var apple = "Apple";
-var cherry = "Cherry";
-var banana = "Banana";
-
-// var fruits = new Array()
-var fruits = ["Apple","Cherry","Banana","Melon","Grapes"];
-// console.log(typeof fruits)
-// console.log(fruits[0])
-// console.log(fruits[1])
-// console.log(fruits[2])
-// console.log(fruits[3])
-// console.log(fruits[4])
-
-// console.log("Jumlah data dalam array : ", fruits.length)
-// console.log("Jumlah huruf " + fruits[0] + " adalah : " + fruits[0].length)
-// console.log("Jumlah huruf " + fruits[1] + " adalah : " + fruits[1].length)
-// console.log("Jumlah huruf " + fruits[2] + " adalah : " + fruits[2].length)
-// console.log("Jumlah huruf " + fruits[3] + " adalah : " + fruits[3].length)
-// console.log("Jumlah huruf " + fruits[4] + " adalah : " + fruits[4].length)
-
-var arrayInArray = [
-    ["Apple","Cherry"],
-    ["Potato","Broccoli"],
-    ["Meat","Fish"]
 ]
+// console.log(cities[0]["citiesCode"])
+// console.log(cities[0].citiesName)
 
-var arrayInObject = {
-    fruits : ["Apple","Cherry"],
-    veggies : ["Potato","Broccolo"],
-    meats : ["Meat","Fish"]
+// output
+var citiesNew = {
+    1: "Bangkok",
+    2: "Jakarta"
 }
-// console.log(arrayInArray[0][0])
-// console.log(arrayInObject.fruits[0])
 
-// console.log(1 || null && 5)
+var citiesNew = {}
+for(let i=0;i<cities.length; i++){
+    citiesNew[cities[i].citiesCode] = cities[i].citiesName;
+}
+console.log(citiesNew);
---
+++ Tasks/Week 2/Thursday/README.md
@@ -0,0 +1,73 @@
+# Exercise 
+
+## 1. Convert Minute to time
+```javascript
+    function convertMinute(){
+
+    }
+
+    //Test Case
+    console.log(convertMinute(100)) //1:40
+    console.log(convertMinute(185)) //3:05
+```
+
+## 2. Print the n first numbers
+```javascript
+    //Test Case
+
+    //Input 15
+    Output
+    1 
+    2
+    3 kelipatan 3
+    4
+    5 kelipatan 5
+    6 kelipatan 3
+    7
+    8
+    9 kelipatan 3
+    10 kelipatan 5
+    .
+    .
+    .
+    15 kelipatan 3 dan 5
+
+```
+## 3. Print Segitiga 
+```javascript
+    //Input 3
+    //Output
+    *
+    **
+    ***
+    
+    // 5
+    //Output
+     *
+     **
+     ***
+     ****
+     *****
+```
+
+## 4. Split words without function .split(" ")
+```javascript 
+    var string = "Lorem ipsum is dummy text"
+    
+    // Using For
+
+    // Using While
+    
+    // Oupu
+    ["Lorem","ipsum","is","dummy","text"]
+```
+
+## 5. Find the faktorial
+```javascript
+    //Faktorial 
+    3! = 3.2.1 = 6
+
+    //Input 5
+    //Output
+    5.4.3.2.1 = 120
+```
