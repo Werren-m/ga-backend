ce43c8df01f0bfb2517abcd82263a6a590cb3974
werren-m <w_erren@hotmail.com>
2020-09-11 00:42:54 +0700

    add thursday task

---
+++ Tasks/Week 3/Thursday/Barrack.js
@@ -0,0 +1,39 @@
+class Barrack {
+    constructor(slots){
+        this.slots = slots || []
+    }
+    group = {
+        Knight: [],
+        Archer: [],
+        Spearman: []
+    };
+    recruit(army){
+        this.slots.push(army);        
+    }
+    summon(){
+        console.log(this.slots);                         
+    }
+    disband(name){
+        for(let i=0; i<this.slots.length; i++){
+            if(this.slots[i].name === name){
+                this.slots.splice(i,1);
+            }
+        }        
+    }
+    groupUp(){
+        for(let i=0; i<this.slots.length; i++){
+            if(this.slots[i].type === "Knight"){
+                this.group.Knight.push(this.slots[i].name)
+            }else if(this.slots[i].type === "Archer"){
+                this.group.Archer.push(this.slots[i].name)
+            }else if(this.slots[i].type === "Spearman"){
+                this.group.Spearman.push(this.slots[i].name)
+            }else{
+                
+            }            
+        }
+        console.log(this.group);
+    }
+}
+
+module.exports = Barrack;
